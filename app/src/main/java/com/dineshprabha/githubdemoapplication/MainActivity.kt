eec9cb853f23ea9fc520af6f59bba5bc43bc8f8d
Dinesh N <dinesh.prabha1205@gmail.com>
2024-06-22 17:45:47 +0530

    Added toast message to click me button

--- app/src/main/java/com/dineshprabha/githubdemoapplication/MainActivity.kt
+++ app/src/main/java/com/dineshprabha/githubdemoapplication/MainActivity.kt
@@ -2,10 +2,27 @@ package com.dineshprabha.githubdemoapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.dineshprabha.githubdemoapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.ClickMe.setOnClickListener {
+
+            Toast.makeText(this, "Hey There...", Toast.LENGTH_SHORT).show()
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/com/dineshprabha/githubdemoapplication/MyApplication.kt
@@ -0,0 +1,6 @@
+package com.dineshprabha.githubdemoapplication
+
+import android.app.Application
+
+class MyApplication: Application() {
+}
